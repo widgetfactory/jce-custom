c8592c9200263fd5d72a3349256a4bdac06658ff
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2020-10-19 11:07:02 +0100

    Update with multiple buttons

--- plugins/jce/editor-custom/editor_plugin.js
+++ plugins/jce/editor-custom/editor_plugin.js
@@ -1,11 +1,20 @@
 (function () {
     tinymce.create('tinymce.plugins.CustomHTML', {
         init: function (ed, url) {
-            ed.addButton('custom', {
-                title: 'Custom HTML',
-                image: url + '/img/custom.png',
+            ed.addButton('custom1', {
+                title: 'Custom HTML 1',
+                image: url + '/img/custom1.png',
                 onclick: function () {
-                    var html = '<span class="custom">Some HTML</span>';
+                    var html = '<span class="custom1">Some HTML</span>';
+                    
+                    ed.execCommand('mceInsertContent', false, html);
+                }
+            });
+            ed.addButton('custom2', {
+                title: 'Custom HTML 2',
+                image: url + '/img/custom2.png',
+                onclick: function () {
+                    var html = '<span class="custom2">Some More HTML</span>';
                     
                     ed.execCommand('mceInsertContent', false, html);
                 }
